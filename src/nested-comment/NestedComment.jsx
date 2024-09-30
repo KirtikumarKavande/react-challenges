6621fa2c16757e49ad47e0f279224ca4d9dc0fff
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-09-30 13:55:13 +0530

    nested comment msg

--- src/nested-comment/NestedComment.jsx
+++ src/nested-comment/NestedComment.jsx
@@ -3,35 +3,7 @@ import AnotherComment from "./AnotherComment";
 
 const NestedComment = () => {
   const [newCommentText, setNewCommentText] = useState("");
-  const [allComments, setAllComments] = useState([
-    {
-      id: 1,
-      msg: "sachin",
-      children: [
-        {
-          id: 2,
-          msg: "virat",
-          children: [
-            {
-              id: 3,
-              msg: "rohit",
-              children: [{ id: 4, msg: "gautam", children: [] }],
-            },
-            {
-              id: 5,
-              msg: "mishra",
-              children: [],
-            },
-          ],
-        },
-        {
-          id: 6,
-          msg: "gavaskar",
-          children: [],
-        }
-      ],
-    },
-  ]);
+  const [allComments, setAllComments] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
